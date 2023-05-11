4f0a04ded4fb48c5e7ed5e82c2c73655e4c2243a
ɴɪssʜᴜ ッ <94668516+n1sshu@users.noreply.github.com>
2023-05-11 17:24:48 +0530

    Update runLinux.sh

--- runLinux.sh
+++ runLinux.sh
@@ -3,4 +3,4 @@ set MODNAME=lostS_pm
 set MAPNAME=mp_killhouse
 
 cd ..\..\
-cod4x18_dedrun +set sv_punkbuster 0 +set pb_sv_enable 0 +map_rotate +sv_authorizemode 0 +set fs_game Mods/%MODNAME% +map %MODNAME% +set r_xassetnum "material=2560 xmodel=1200 xanim=3200 image=3000" +set promod_mode custom_public + exec server.cfg +set g_gametype sd
+cod4x18_dedrun +set sv_punkbuster 0 +set pb_sv_enable 0 +map_rotate +sv_authorizemode 0 +set fs_game Mods/%MODNAME% +map %MAPNAME% +set r_xassetnum "material=2560 xmodel=1200 xanim=3200 image=3000" +set promod_mode custom_public + exec server.cfg +set g_gametype sd
